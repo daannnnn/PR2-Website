ea03379856c41bd1c332f6cd244596890d28f191
Dan Artillaga <artillagadan@gmail.com>
2022-01-08 20:06:12 +0800

    update constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,16 +4,36 @@ const String CURRENT = 'a';
 const String TOTAL = 'b';
 const String VALUES = 'c';
 const String DATE = 'd';
-const String INTERVAL_VALUE = 'e';
+const String LIST = 'e';
 
-const String AIR_HUMIDITY = 'f';
-const String AIR_TEMPERATURE = 'g';
-const String SOIL_TEMPERATURE = 'h';
-const String SOIL_MOISTURE = 'i';
-const String LIGHT = 'j';
+const AIR_HUMIDITY = "f";
+const AIR_TEMPERATURE = "g";
+const SOIL_TEMPERATURE = "h";
+const SOIL_MOISTURE = "i";
+const LIGHT = "j";
 
-const String COUNT = 'k';
+const COUNT = "k";
 
-const String PREFERENCES = 'l';
-const String DEVICE_PREFERENCES = 'm';
-const String UPDATE_INTERVAL = 'n';
+const PREFERENCES = "l";
+const DEVICE_PREFERENCES = "m";
+
+const USER_PREF = "n";
+
+const ALERTS = "o";
+const FACTOR = "p";
+const VALUE = "q";
+const ON_INCREASE = "r";
+const ALERT_VALUE = "u";
+
+const TOKENS = "s";
+const TOKEN = "t";
+
+const UPDATE_FLAGS = "v";
+
+const UPDATE_INFO = "w";
+
+const UPDATE_INTERVAL = "x";
+
+const UPDATE_FAILED = "y";
+
+const ALERT_LIST = "z";
